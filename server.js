05573ef1c309f8dba11f870e74d0cf6529ae8f48
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-04-12 01:05:25 +0200

    last

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
+console.log(express.static('./dist/'));
 app.use(express.static('./dist/'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/'}),
+    res.sendFile('index.html', {root: '/'}),
 );
 
 // Start the app by listening on the default Heroku port
