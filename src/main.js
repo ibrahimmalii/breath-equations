d1a3f55cb59183995882aa713d49cd5ff15cf891
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-04-11 08:22:32 +0200

    latest

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@ import App from './App.vue';
 import { createWebHistory, createRouter } from 'vue-router';
 import IdealBody from './components/pages/IdealBody.vue';
 import HomePage from './components/pages/HomePage.vue';
-// import oxygenation from './components/pages/oxygenation.vue';
-// import conservate from './components/pages/Conservate.vue';
-// import support from './components/pages/Support.vue';
+import oxygenation from './components/pages/oxygenation.vue';
+import conservate from './components/pages/Conservate.vue';
+import support from './components/pages/Support.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/ventilatioon-parameters', component: IdealBody },
-  // { path: '/oxygenation-measurements', component: oxygenation },
-  // { path: '/conservative-therapy', component: conservate },
-  // { path: '/support', component: support },
+  { path: '/oxygenation-measurements', component: oxygenation },
+  { path: '/conservative-therapy', component: conservate },
+  { path: '/support', component: support },
 ];
 
 const router = createRouter({
