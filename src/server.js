eea95e318952b5c813c8368bc9addcc38d1056da
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-04-11 08:01:40 +0200

    update

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+console.log(path.join(__dirname, 'dist'))
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 8000;
 app.listen(port);
