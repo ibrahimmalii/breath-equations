479bcdd2874be375de170bf04ff0f77ff669adf4
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2022-04-12 00:53:23 +0200

    update server

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const serveStatic = require('serve-static');
 const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-app.listen(process.env.PORT || 8080);
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
+app.listen(port);
